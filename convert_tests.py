9be2b45766d2c693d9a9305dd11847aa6d15faf3
Hikatchus <76414030+Hikatchus@users.noreply.github.com>
2024-11-11 21:37:24 -0800

    All done!

--- convert_tests.py
+++ convert_tests.py
@@ -1,1 +1,13 @@
+from convert import str_to_float
+import unittest
+
+class TestCases(unittest.TestCase):
+    def test_str_to_float_1(self):
+        self.assertEqual(str_to_float('123'), 123.0)
+    def test_str_to_float_2(self):
+        self.assertEqual(str_to_float('Hello!'), None)
+
+
+if __name__ == '__main__':
+    unittest.main()
 
--- keyboard.py
+++ keyboard.py
@@ -1,1 +1,18 @@
+from convert import str_to_float
 
+
+def gather_numbers() -> list[float]:
+    output = []
+    continue_flag = True
+    while continue_flag:
+        user_input = input("Enter a number, or 'done'")
+        if user_input == 'done':
+            continue_flag = False
+        elif str_to_float(user_input) == None:
+            pass
+        else:
+            output.append(str_to_float(user_input))
+    return output
+
+if __name__ == '__main__':
+    print(gather_numbers())
